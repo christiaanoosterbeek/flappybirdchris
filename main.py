dd2f0634d0d5d8e93193a6e242a1d6c5c50c2f9f
christiaanoosterbeek <57255367+christiaanoosterbeek@users.noreply.github.com>
2023-04-10 14:52:38 +0200

    game variabelen toegevoegd

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy Bird")
 
 clock = pygame.time.Clock()
+
+gravity = 0.5
+bird_y = 200
+bird_dy = 0
+pipe_x = 500
+pipe_dy = -3
+gap_size = 150
+pipe_width = 50
+pipe_gap_y = 300
+score = 0
+font = pygame.font.SysFont(None, 40)
